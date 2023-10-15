021814bcd4ecd7ad88bc3c90f78f07d83c5cc30a
Vibin333 <132742732+Vibin333@users.noreply.github.com>
2023-10-15 22:27:49 +0530

    Add files via upload

--- Day 4/day4webscrape.py
+++ Day 4/day4webscrape.py
@@ -8,14 +8,18 @@ heading_list=[]
 headings=soup.find_all(class_='titleline')
 for heading in headings:
     heading_list.append(heading.text)
+print('All headings:')
 for i in heading_list:
     print(i)
+print('\n')
 scores=soup.find_all(class_='score')
 score_list=[]
 for score in scores:
     score_list.append(score.text)
+print("All scores:")
 for i in score_list:
     print(i)
+print('\n')
 link_list=[]
 alllinks=soup.find_all('span',class_='titleline')
 for links in alllinks:
@@ -23,5 +27,6 @@ for links in alllinks:
     for l in link:
         url=l.get('href')
         link_list.append(url)
+print("All links:")
 for i in link_list:
     print(i)
